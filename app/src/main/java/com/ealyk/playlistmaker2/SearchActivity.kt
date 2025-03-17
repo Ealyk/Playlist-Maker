eccca3bddc069a8a0e9d5b849d903fa004083454
Ealyk <vip.vuyalkin@gmail.com>
2025-03-17 17:47:56 +0300

    добавление взаимодействия с iTunesApi

--- app/src/main/java/com/ealyk/playlistmaker2/SearchActivity.kt
+++ app/src/main/java/com/ealyk/playlistmaker2/SearchActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
@@ -13,24 +14,48 @@ import android.widget.FrameLayout
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
 
     private var savedEditText = DEF_TEXT
 
+    private val iTunesbaseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesbaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(iTunesApi::class.java)
+
+    private lateinit var backButton: Button
+    private lateinit var editTextSearch: EditText
+    private lateinit var clearButton: FrameLayout
+    private lateinit var rootLayout: LinearLayout
+    private lateinit var rvTrackSearch: RecyclerView
+    private lateinit var reloadButton: Button
+
+    private val trackList = ArrayList<Track>()
+
+    private val adapter = TrackAdapter(trackList)
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-
-        val backButton = findViewById<Button>(R.id.back_button)
-        val editTextSearch = findViewById<EditText>(R.id.searching)
-        val clearButton = findViewById<FrameLayout>(R.id.clear_button)
-        val rootLayout = findViewById<LinearLayout>(R.id.search_layout)
-        val rvTrackSearch = findViewById<RecyclerView>(R.id.recyclerTrackSearch)
+        backButton = findViewById(R.id.back_button)
+        editTextSearch = findViewById(R.id.searching)
+        clearButton = findViewById(R.id.clear_button)
+        rootLayout = findViewById(R.id.search_layout)
+        rvTrackSearch = findViewById(R.id.recyclerTrackSearch)
+        reloadButton = findViewById(R.id.reloadButton)
 
         if (savedInstanceState != null) {
             savedEditText = savedInstanceState.getString(EDITABLE_TEXT, DEF_TEXT)
@@ -44,6 +69,9 @@ class SearchActivity : AppCompatActivity() {
 
         clearButton.setOnClickListener {
             editTextSearch.setText("")
+            trackList.clear()
+            adapter.notifyDataSetChanged()
+            showTrackList()
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(rootLayout.windowToken, 0)
         }
@@ -68,41 +96,50 @@ class SearchActivity : AppCompatActivity() {
         }
         editTextSearch.addTextChangedListener(textWatcher)
 
-        rvTrackSearch.adapter = TrackAdapter(
-            listOf (
-                Track (
-                    "Smells Like Teen Spirit",
-                    "Nirvana",
-                    "5:01",
-                    getString(R.string.Smells_Like_Teen_Spirit_uri)
-                ),
-                Track (
+        rvTrackSearch.adapter = adapter
 
-                    "Billie Jean",
-                    "Michael Jackson",
-                    "4:35",
-                    getString(R.string.Billie_Jean_uri)
-                ),
-                Track (
-                    "Stayin' Alive",
-                    "Bee Gees",
-                    "4:10",
-                    getString(R.string.Stayin_Alive_uri)
-                ),
-                Track (
-                    "Whole Lotta Love",
-                    "Led Zeppelin",
-                    "5:33",
-                    getString(R.string.Whole_Lotta_Love_uri)
-                ),
-                Track (
-                    "Sweet Child O'Mine",
-                    "Guns N' Roses",
-                    "5:03",
-                    getString(R.string.Sweet_Child_uri)
-                )
-            )
-        )
+        editTextSearch.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (editTextSearch.text.isNotEmpty()) {
+                    iTunesService.search(editTextSearch.text.toString()).enqueue(object : Callback<TrackResponse>{
+                        override fun onResponse(
+                            call: Call<TrackResponse>,
+                            response: Response<TrackResponse>
+                        ) {
+                            if (response.code() == 200) {
+                                trackList.clear()
+                                if (response.body()?.results?.isNotEmpty() == true) {
+                                    trackList.addAll(response.body()?.results!!)
+                                    adapter.notifyDataSetChanged()
+                                }
+                                if (trackList.isEmpty()) {
+                                    showEmptyState()
+                                }
+                                else showTrackList()
+                            }
+                            else {
+                                showErrorState()
+                                reloadButton.setOnClickListener {
+
+                                    recreate()
+                                }
+                            }
+                        }
+
+                        override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                            showErrorState()
+                            reloadButton.setOnClickListener {
+
+                                recreate()
+                            }
+                        }
+
+                    })
+                }
+                true
+            }
+            false
+        }
 
 
     }
@@ -125,8 +162,24 @@ class SearchActivity : AppCompatActivity() {
             View.VISIBLE
 
         }
+    }
 
+    private fun showEmptyState() {
+        findViewById<LinearLayout>(R.id.emptyStateLayout).visibility = View.VISIBLE
+        findViewById<LinearLayout>(R.id.errorStateLayout).visibility = View.GONE
+        findViewById<RecyclerView>(R.id.recyclerTrackSearch).visibility = View.GONE
+    }
 
+    private fun showErrorState() {
+        findViewById<LinearLayout>(R.id.emptyStateLayout).visibility = View.GONE
+        findViewById<LinearLayout>(R.id.errorStateLayout).visibility = View.VISIBLE
+        findViewById<RecyclerView>(R.id.recyclerTrackSearch).visibility = View.GONE
+    }
+
+    private fun showTrackList() {
+        findViewById<LinearLayout>(R.id.emptyStateLayout).visibility = View.GONE
+        findViewById<LinearLayout>(R.id.errorStateLayout).visibility = View.GONE
+        findViewById<RecyclerView>(R.id.recyclerTrackSearch).visibility = View.VISIBLE
     }
 
     companion object {
